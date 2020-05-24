37c7cc39b0350123a4b1b4bf39d82348f09e7adc
rayli1639 <ray.z.li@gmail.com>
2020-05-24 19:27:29 -0400

    Added templates

---
+++ README.md
@@ -0,0 +1,1 @@
+Learning Djano by creating a web application
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Raymond',
+        'title': 'My Story',
+        'content': 'First Post Content',
+        'date_posted': 'May 24, 2020'
+    },
+    {
+        'author': 'Joe',
+        'title': 'Joe\'s Story',
+        'content': 'Joe First Post Content',
+        'date_posted': 'May 25, 2020'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context) 
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
+    context = {'title': 'About'}
+    return render(request, 'blog/about.html', context)
